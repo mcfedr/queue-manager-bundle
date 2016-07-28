0fdf7d6b23e4931a38384f993979d57b53d9ae4b
Fred Cox <fred@ekreative.com>
2016-07-28 15:06:46 +0300

    Add missing return

--- src/Mcfedr/QueueManagerBundle/Manager/QueueManagerRegistry.php
+++ src/Mcfedr/QueueManagerBundle/Manager/QueueManagerRegistry.php
@@ -18,17 +18,32 @@ class QueueManagerRegistry
      */
     private $default;
 
+    /**
+     * @param array $queueManagers
+     * @param string $default
+     */
     public function __construct(array $queueManagers, $default)
     {
         $this->queueManagers = $queueManagers;
         $this->default = $default;
     }
 
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @param array $options
+     * @param string $manager
+     * @return Job
+     */
     public function put($name, array $arguments = [], array $options = [], $manager = null)
     {
-        $this->queueManagers[$manager ?: $this->default]->put($name, $arguments, $options);
+        return $this->queueManagers[$manager ?: $this->default]->put($name, $arguments, $options);
     }
 
+    /**
+     * @param Job $job
+     * @param string $manager
+     */
     public function delete(Job $job, $manager = null)
     {
         $this->queueManagers[$manager ?: $this->default]->delete($job);
