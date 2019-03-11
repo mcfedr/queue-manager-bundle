1b1559b3a4aaf4a03658140fc7e008a4db265519
Fred Cox <mcfedr@gmail.com>
2019-03-11 11:58:48 +0200

    cs

--- src/Mcfedr/QueueManagerBundle/Subscriber/MemoryReportSubscriber.php
+++ src/Mcfedr/QueueManagerBundle/Subscriber/MemoryReportSubscriber.php
@@ -46,8 +46,8 @@ class MemoryReportSubscriber implements EventSubscriberInterface
     private function report(): void
     {
         $this->logger->info('Memory after job', [
-            'usage KB:' => round(\memory_get_usage(true) / 1024),
-            'peak KB:' => round(\memory_get_peak_usage(true) / 1024),
+            'usage KB:' => round(memory_get_usage(true) / 1024),
+            'peak KB:' => round(memory_get_peak_usage(true) / 1024),
         ]);
     }
 }
